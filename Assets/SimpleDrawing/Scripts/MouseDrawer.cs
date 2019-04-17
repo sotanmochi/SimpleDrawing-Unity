9b44d7daa9c9ae8df470c79a67592efec8f7cbb9
sotanmochi <stmczk@gmail.com>
2019-04-17 18:31:30 +0900

    Initial commit

---
+++ Assets/SimpleDrawing/Scripts/MouseDrawer.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace SimpleDrawing
+{
+    public class MouseDrawer : MonoBehaviour
+    {
+        [SerializeField]
+        Color penColor = Color.red;
+
+        [SerializeField]
+        int penWidth = 3;
+
+        [SerializeField]
+        bool erase = false;
+
+        Vector2 previousTexCoord;
+        bool previousMouseDown;
+
+        void Update()
+        {
+            bool mouseDown = Input.GetMouseButton(0);
+            if (mouseDown)
+            {
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hitInfo;
+				if(Physics.Raycast(ray, out hitInfo))
+                {
+                    if(hitInfo.collider != null && hitInfo.collider is MeshCollider)
+                    {
+                        var drawObject = hitInfo.transform.GetComponent<DrawableCanvas>();
+                        if (drawObject != null)
+                        {
+                            Vector2 currentTexCoord = hitInfo.textureCoord;
+                            if (erase)
+                            {
+                                drawObject.Erase(currentTexCoord, previousTexCoord, penWidth);
+                            }
+                            else
+                            {
+                                drawObject.Draw(currentTexCoord, previousTexCoord, penWidth, penColor);
+                            }
+                            previousTexCoord = currentTexCoord;
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogWarning("If you want to draw using a RaycastHit, need set MeshCollider for object.");
+                    }
+                }
+            }
+            else if (!mouseDown) // Mouse is released
+            {
+                previousTexCoord = Vector2.zero;
+            }
+            previousMouseDown = mouseDown;
+        }
+    }
+}
