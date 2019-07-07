39f65ac3ab0c381bb6c9294d4db092498111ac49
sotanmochi <stmczk@gmail.com>
2019-07-07 22:13:31 +0900

    [WIP] Implement drawing line using shader

--- Assets/SimpleDrawing/Scripts/DrawableCanvas.cs
+++ Assets/SimpleDrawing/Scripts/DrawableCanvas.cs
@@ -100,26 +100,81 @@ namespace SimpleDrawing
             int width = drawableTexture.width;
             int height = drawableTexture.height;
 
-            // Get the distance from start to end
-            float distance = Vector2.Distance(startPixelPos, endPixelPos);
-            Vector2 direction = (startPixelPos - endPixelPos).normalized;
+            Vector2 a = new Vector2((int)startPixelPos.x, (int)startPixelPos.y);
+            Vector2 b = new Vector2((int)endPixelPos.x, (int)endPixelPos.y);
 
-            // Calculate how many times we should interpolate 
-            // between start point and end point based on 
-            // the amount of time that has passed since the last update.
-            float lerpSteps = 1 / distance;
-            Vector2 currentPosition = startPixelPos;
-            for (float lerp = 0; lerp <= 1; lerp += lerpSteps)
+            int dx = (b.x - a.x) > 0 ? 1 : -1;
+            int dy = (b.y - a.y) > 0 ? 1 : -1;
+            // *********************
+            //  CPU implementation
+            // *********************
+            for (int py = 0; py < height; py++)
+            // for (int py = (int)a.y; py != (int)b.y; py += dy)
             {
-                currentPosition = Vector2.Lerp(startPixelPos, endPixelPos, lerp);
-                MarkPixelsToColor(currentPosition, thickness, color);
+                for (int px = 0; px < width; px++)
+                // for (int px = (int)a.x; px != (int)b.x; px += dx)
+                {
+                    // *********************************************************
+                    //  Solve a system of linear equations using Cramer's rule
+                    //  The system of linear equations:
+                    //    px = s*ax + t*bx
+                    //    py = s*ay + t*by
+                    //    Unknowns: s,t
+                    // *********************************************************
+                    float determinant = a.x*b.y - b.x*a.y;
+                    if (!Mathf.Approximately(determinant, 0.0f))
+                    {
+                        float s = (px*b.y - b.x*py) / determinant;
+                        float t = (a.x*py - px*a.y) / determinant;
+
+                        // Whether a point (px,py) is on the line segment AB.
+                        if (GreaterThanEqualApproximately(s, 0.0f) &&
+                            GreaterThanEqualApproximately(t, 0.0f) &&
+                            Mathf.Approximately((s + t), 1.0f))
+                        {
+                            float d = Mathf.Sqrt((b.y - a.y)*(b.y - a.y) + (b.x - a.x)*(b.x - a.x));
+                            if (d > 0.0f)
+                            {
+                                d = 1.0f/d * Mathf.Abs((b.y - a.y)*px - (b.x - a.x)*py + (b.x*a.y - b.y*a.x));
+                            }
+
+                            // if (d < 0.5f * Mathf.Sqrt(2.0f) )
+                            if (d < 1.0f )
+                            {
+                                MarkPixelsToColor(new Vector2(px, py), thickness, color);
+                            }
+                        }
+                    }
+                }
             }
+
+            // ***********************************************************************
+            // // Get the distance from start to end
+            // float distance = Vector2.Distance(startPixelPos, endPixelPos);
+            // Vector2 direction = (startPixelPos - endPixelPos).normalized;
+
+            // // Calculate how many times we should interpolate 
+            // // between start point and end point based on 
+            // // the amount of time that has passed since the last update.
+            // float lerpSteps = 1 / distance;
+            // Vector2 currentPosition = startPixelPos;
+            // for (float lerp = 0; lerp <= 1; lerp += lerpSteps)
+            // {
+            //     currentPosition = Vector2.Lerp(startPixelPos, endPixelPos, lerp);
+            //     MarkPixelsToColor(currentPosition, thickness, color);
+            // }
+            // ***********************************************************************
         }
 
         public void ResetCanvas()
         {
             drawableTexture.SetPixels32(resetColorPixels);
             drawableTexture.Apply();
+        }
+
+        private bool GreaterThanEqualApproximately(float a, float b)
+        {
+            return (a > b) || Mathf.Approximately(a, b);
         }
     }
 }
