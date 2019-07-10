2c3ce8633d970939d174522907f72e739f1add17
sotanmochi <stmczk@gmail.com>
2019-07-10 19:05:59 +0900

    Update

--- Assets/SimpleDrawing/Scripts/DrawableCanvas.cs
+++ Assets/SimpleDrawing/Scripts/DrawableCanvas.cs
@@ -6,22 +6,26 @@ namespace SimpleDrawing
 	[DisallowMultipleComponent]
     public class DrawableCanvas : MonoBehaviour
     {
+        public bool ResetCanvasOnPlay = true;
         public Color ResetColor = new Color(1, 1, 1, 1);
 
         private Material drawLineMaterial = null;
+        private Material singleColorMaterial = null;
         private RenderTexture drawableRenderTexture;
 
         #region ShaderPropertyID
 		private int mainTexturePropertyID;
-		private int colorPropertyID;
+		private int lineColorPropertyID;
 		private int thicknessPropertyID;
 		private int startPositionUVPropertyID;
 		private int endPositionUVPropertyID;
+		private int singleColorPropertyID;
         #endregion
 
         private void Awake()
         {
             drawLineMaterial = new Material(Resources.Load<Material>("SimpleDrawing.DrawLine"));
+            singleColorMaterial = new Material(Resources.Load<Material>("SimpleDrawing.SingleColor"));
             InitializePropertyID();
         }
 
@@ -32,11 +36,15 @@ namespace SimpleDrawing
 
         private void InitializePropertyID()
         {
+            // DrawLine.shader
 			mainTexturePropertyID = Shader.PropertyToID("_MainTex");
-			colorPropertyID = Shader.PropertyToID("_LineColor");
+			lineColorPropertyID = Shader.PropertyToID("_LineColor");
 			thicknessPropertyID = Shader.PropertyToID("_Thickness");
 			startPositionUVPropertyID = Shader.PropertyToID("_StartPositionUV");
 			endPositionUVPropertyID = Shader.PropertyToID("_EndPositionUV");
+
+            // SingleColor.shader
+            singleColorPropertyID = Shader.PropertyToID("_SingleColor");
         }
 
         private void InitializeDrawCanvas()
@@ -48,12 +56,17 @@ namespace SimpleDrawing
 			drawableRenderTexture.filterMode = mainTexture.filterMode;
 			Graphics.Blit(mainTexture, drawableRenderTexture);
             material.mainTexture = drawableRenderTexture;
+
+            if (ResetCanvasOnPlay)
+            {
+                ResetCanvas();
+            }
         }
 
         public void Draw(Vector2 currentTexCoord, Vector2 previousTexCoord, int thickness, Color color)
         {
             drawLineMaterial.SetInt(thicknessPropertyID, thickness);
-            drawLineMaterial.SetVector(colorPropertyID, color);
+            drawLineMaterial.SetVector(lineColorPropertyID, color);
             drawLineMaterial.SetVector(startPositionUVPropertyID, previousTexCoord);
             drawLineMaterial.SetVector(endPositionUVPropertyID, currentTexCoord);
 
@@ -68,9 +81,14 @@ namespace SimpleDrawing
             Draw(currentTexCoord, previousTexCoord, thickness, ResetColor);
         }
 
-        private bool GreaterThanEqualApproximately(float a, float b)
+        public void ResetCanvas()
         {
-            return (a > b) || Mathf.Approximately(a, b);
+            singleColorMaterial.SetVector(singleColorPropertyID, ResetColor);
+
+			var mainPaintTextureBuffer = RenderTexture.GetTemporary(drawableRenderTexture.width, drawableRenderTexture.height, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
+            Graphics.Blit(drawableRenderTexture, mainPaintTextureBuffer, singleColorMaterial);
+            Graphics.Blit(mainPaintTextureBuffer, drawableRenderTexture);
+            RenderTexture.ReleaseTemporary(mainPaintTextureBuffer);
         }
     }
 }
